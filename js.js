8606a89a673e4b800ab496155958641c2d051a37
Carlos Manuel Chee Vázquez <carlos.chee@cobachbc.edu.mx>
2025-01-14 11:49:55 -0800

    carga inicial

---
+++ js.js
@@ -0,0 +1,58 @@
+function stop(){
+    document.querySelectorAll('audio').forEach(el => el.pause());
+    document.querySelectorAll('audio').forEach(el => el.currentTime = 0);
+}
+
+function playa(){
+    let audioa = document.getElementById("aa");
+
+    document.querySelectorAll('audio').forEach(el => el.pause());
+
+    audioa.currentTime = 0;
+    audioa.play();
+}
+
+function playb(){
+    let audiob = document.getElementById("bb");
+
+    document.querySelectorAll('audio').forEach(el => el.pause());
+
+    audiob.currentTime = 0;
+    audiob.play();
+}
+
+function playc(){
+    let audioc = document.getElementById("cc");
+
+    document.querySelectorAll('audio').forEach(el => el.pause());
+
+    audioc.currentTime = 0;
+    audioc.play();
+}
+
+function playd(){
+    let audiod = document.getElementById("dd");
+
+    document.querySelectorAll('audio').forEach(el => el.pause());
+
+    audiod.currentTime = 0;
+    audiod.play();
+}
+
+function playe(){
+    let audioe = document.getElementById("ee");
+
+    document.querySelectorAll('audio').forEach(el => el.pause());
+
+    audioe.currentTime = 0;
+    audioe.play();
+}
+
+function playf(){
+    let audiof = document.getElementById("ff");
+
+    document.querySelectorAll('audio').forEach(el => el.pause());
+
+    audiof.currentTime = 0;
+    audiof.play();
+}
